9841cd7930f2d72fbbd6f5ae467fee795a1cc35d
lyxal <36217120+Lyxal@users.noreply.github.com>
2025-07-14 11:39:52 +1000

    Also silence default number watch for consistency

--- WatchScribe.user.js
+++ WatchScribe.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         WatchScribe
-// @version      0.8.0
+// @version      0.8.1
 // @description  A userscript to help generate regexes for SmokeDetector's watchlist feature. To be used in conjunction with FIRE.
 // @author       lyxal
 // @homepage     https://github.com/lyxal/WatchScribe
@@ -309,7 +309,7 @@
             regexes.push(`!!/${commandType}-number- ${justNumbers.slice(1)} (?#NO NorAm)`);
         }
 
-        regexes.push(`!!/${commandType}-number ${justNumbers}`);
+        regexes.push(`!!/${commandType}-number- ${justNumbers}`);
 
 
         return regexes;
