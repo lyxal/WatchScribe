6b4e31ef378d4716384405a0b4e2d8bce057710d
lyxal <36217120+Lyxal@users.noreply.github.com>
2025-07-18 12:05:06 +1000

    Version bump

--- WatchScribe.user.js
+++ WatchScribe.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         WatchScribe
-// @version      0.12.0
+// @version      0.12.1
 // @description  A userscript to help generate regexes for SmokeDetector's watchlist feature. To be used in conjunction with FIRE.
 // @author       lyxal
 // @homepage     https://github.com/lyxal/WatchScribe
